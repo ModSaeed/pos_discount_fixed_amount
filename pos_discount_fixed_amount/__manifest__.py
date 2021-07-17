a04a9384b5d6703c976ebb58ef6447287a46aac5
ModSaeed <mohammedsaeed939@gmail.com>
2021-07-17 12:42:14 +0400

    First upload

---
+++ pos_discount_fixed_amount/__manifest__.py
@@ -0,0 +1,18 @@
+
+{
+    "name": "Point of Sale Fixed amount Discounts",
+    "summary": "Allow to apply discounts with fixed amount on total order",
+    "version": "13.0.1",
+    "category": "Point of Sale",
+    "website": "",
+    "author": "Modsaeed",
+    'license': 'OPL-1',
+    'currency': 'EUR',
+    'price': 14.0,
+    "application": False,
+    "installable": True,
+    "depends": ["pos_discount"],
+    "data": ["views/pos_templates.xml"],
+    "qweb": ["static/src/xml/discount_templates.xml"],
+    'images': ['static/description/images/images.jpg','static/description/images/images.jpg'],
+}
